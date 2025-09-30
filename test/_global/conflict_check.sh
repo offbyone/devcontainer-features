267f63562c281987235efdf477a2babecf7d64ea
Chris Rose <offline@offby1.net>
2025-09-30 11:21:58 -0700

    Create the prek devcontainer feature
    - install [j178/prek](https://github.com/j178/prek) from GH releases
    - test that

--- test/_global/conflict_check.sh
+++ test/_global/conflict_check.sh
@@ -3,11 +3,11 @@
 # The 'test/_global' folder is a special test folder that is not tied to a single feature.
 #
 # This test file is executed against a running container constructed
-# from the value of 'color_and_hello' in the tests/_global/scenarios.json file.
+# from the value of 'prek' in the tests/_global/scenarios.json file.
 #
 # The value of a scenarios element is any properties available in the 'devcontainer.json'.
 # Scenarios are useful for testing specific options in a feature, or to test a combination of features.
-# 
+#
 # This test can be run with the following command (from the root of this repo)
 #    devcontainer features test --global-scenarios-only .
 
@@ -16,17 +16,13 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-echo -e "The result of the 'color' command will be:\n"
-color
-echo -e "The result of the 'hello' command will be:\n"
-hello
+echo -e "The result of the 'prek --version' command will be:\n"
+prek --version
 echo -e "\n"
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "check purple is my favorite color" bash -c "color | grep 'my favorite color is purple'"
-check "check I am greeting with 'Greetings'" bash -c "hello | grep 'Greetings, $(whoami)'"
-
+check "check prek" bash -c "prek --version | grep -E '^prek'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
